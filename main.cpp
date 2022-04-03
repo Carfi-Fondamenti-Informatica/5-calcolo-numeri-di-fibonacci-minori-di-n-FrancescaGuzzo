cbfeffc991adc55e7b663a0999f0f4a259f34294
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-04-03 18:18:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main (){
      int n, p=1, s=0, F;
-    cout << "inserire un valore intero: ";
+    cout << "inserire un valore intero: " << endl;
     cin >> n;
 
     for (int i=0; i<n; i++){
